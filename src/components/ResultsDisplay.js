481bf8f692121f8ab7b97731cfb96c428b8b84b4
pete <pkeen7@gmail.com>
2023-05-09 21:41:05 -0600

    refactor ResultsDisplay to render conditionally

--- src/components/ResultsDisplay.js
+++ src/components/ResultsDisplay.js
@@ -6,43 +6,28 @@ import { styled } from '@mui/material/styles';
 function ResultsDisplay({currencyComparison, currency1, currency2, multiplier, multiplier2}) {
      /* Currency Comparisons */
   // const currencyComparison = useCompareCurrencies(selectedCurrency1, selectedCurrency2);
-  if (!currency1 || !currency2) {
-    return (<></>)
+  if (currency1 && currency2 && multiplier) {
+    return (
+      <Grid item>
+        <Typography 
+          variant='h5' 
+          component='h2'
+        > 
+            <span className="primary">
+            {localFormatCurrency(multiplier)} {currency1.toUpperCase()}
+            </span>
+            {` = `}
+            <span className="secondary">
+             {localFormatCurrency(multiplier2)} {currency2.toUpperCase()}
+            </span> 
+        </Typography>
+      </Grid>
+    )
+  } else {
+    return <></>
   }
-  /* Logic for creating list */
-  let currencyComparisonArray = Object.entries(currencyComparison);
-
-  currencyComparisonArray = currencyComparisonArray.map((currencyComparison) => {
-    return <li key={currencyComparison[0]}>{currencyComparison[1] * multiplier} {currencyComparison[0]} </li>
-  })
-
-  return (
-    <Grid item>
-      <Typography 
-        variant='h5' 
-        component='h2'
-      > 
-          <span className="primary">
-          {localFormatCurrency(multiplier)} {currency1.toUpperCase()}
-          </span>
-          {` = `}
-          <span className="secondary">
-           {localFormatCurrency(multiplier2)} {currency2.toUpperCase()}
-          </span> 
-      </Typography>
-    </Grid>
-        
 
     
-  )
 }
 
 export default ResultsDisplay;
-
-/*
-<br></br>
-        <ul className={styles.secondCurrency}>
-            {currencyComparisonArray}
-        </ul>
-
-        */
